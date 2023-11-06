a9b0a7e3497f7e4d11d39e9707a7d383fa9d5818
winoz <winoz@naver.com>
2023-11-06 16:19:26 +0900

    test view

---
+++ Ex02_/src/main/java/com/example/demo/MyController.java
@@ -0,0 +1,9 @@
+package com.example.demo;
+
+public class MyController {
+	public void Mycontroller() {
+		System.out.println("test");
+		System.out.println("test");
+		System.out.println("test");
+	}
+}
